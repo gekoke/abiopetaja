1675209d1f454f6311be035505f5a5e540e634a9
Gregor Grigorjan <gregor@grigorjan.net>
2024-02-23 13:33:47 +0200

    feat(urls): route root url to `app:dashboard` view

--- abiopetaja/urls.py
+++ abiopetaja/urls.py
@@ -20,11 +20,13 @@ Including another URLconf
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = i18n_patterns(
+    path("", RedirectView.as_view(pattern_name="app:dashboard", permanent=False)),
+    path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),
     path("app/", include("app.urls")),
-    path("accounts/", include("allauth.urls")),
 )
 
 urlpatterns += [
