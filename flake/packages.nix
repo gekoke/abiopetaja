04ab1bad27b38e012dcac9f726c532a62c079b27
Gregor Grigorjan <gregor@grigorjan.net>
2024-03-25 19:07:21 +0200

    refactor(flake): remove superfluous code

--- flake/packages.nix
+++ flake/packages.nix
@@ -7,11 +7,6 @@
         python = pkgs.python312;
       };
 
-      dependencyEnv = (pkgs.poetry2nix.mkPoetryApplication {
-        projectDir = ../.;
-        python = pkgs.python312;
-      }).dependencyEnv;
-
       provisionSecretsScript = pkgs.writeShellScriptBin "activate" ''
         SECRET_KEY_FILE="/etc/DJANGO_SECRET_KEY.txt"
 
@@ -27,9 +22,9 @@
       '';
 
       migrationScript = pkgs.writeShellScriptBin "activate" ''
-        ${self.packages.${system}.dependencyEnv}/bin/python -m manage makemigrations
-        ${self.packages.${system}.dependencyEnv}/bin/python -m manage migrate
-        ${self.packages.${system}.dependencyEnv}/bin/python -m manage collectstatic --no-input
+        ${self.packages.${system}.default.dependencyEnv}/bin/python -m manage makemigrations
+        ${self.packages.${system}.default.dependencyEnv}/bin/python -m manage migrate
+        ${self.packages.${system}.default.dependencyEnv}/bin/python -m manage collectstatic --no-input
       '';
 
       postMigrationScript = pkgs.writeShellScriptBin "activate" ''
