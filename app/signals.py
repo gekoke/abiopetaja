b1589d7907dab060167dc53f4039252e1c61555a
Gregor Grigorjan <gregor@grigorjan.net>
2024-04-13 21:19:25 +0300

    refactor(signals): use Django built-in signals
    So we can create users more easily in tests
    but still have the signals run

--- app/signals.py
+++ app/signals.py
@@ -1,8 +1,8 @@
 import logging
 
-from allauth.account.signals import user_signed_up
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.translation import gettext as _
 
@@ -11,8 +11,14 @@ from app.models import ProblemKind, Template
 logger = logging.getLogger(__name__)
 
 
-@receiver(user_signed_up)
-def add_default_templates(user: User, **kwargs):
+@receiver(post_save, sender=User)
+def add_default_templates(sender, instance: User, created: bool, **kwargs):
+    if not created:
+        # Not a newly created user
+        return
+
+    user = instance
+
     template = Template()
     template.name = _("Inequalities")
     template.author = user
@@ -23,6 +29,7 @@ def add_default_templates(user: User, **kwargs):
 
     if not settings.DEBUG:
         return
+
     add_test_templates(user)
 
 
