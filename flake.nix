a63275b02930351c3437bdf287ab0993e595004e
Gregor Grigorjan <gregor@grigorjan.net>
2024-04-13 15:20:18 +0300

    tweak(ci/cd): speed up ci/cd by using one `nixpkgs` input

--- flake.nix
+++ flake.nix
@@ -6,10 +6,13 @@
 
     # FIXME: delete when https://github.com/nix-community/poetry2nix/pull/1515 is merged
     poetry2nix.url = "github:gekoke/poetry2nix";
+    poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
 
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
 
     deploy-rs.url = "github:serokell/deploy-rs";
+    deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs: import ./flake inputs;
