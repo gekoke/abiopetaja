28da0f415397b5b050d2c7ea800e248c2a580cb3
Gregor Grigorjan <gregor@grigorjan.net>
2024-04-13 23:17:17 +0300

    refactor: place test dependencies in explicit `pyproject.toml` group

--- flake/checks.nix
+++ flake/checks.nix
@@ -9,7 +9,7 @@
 
     pre-commit =
       let
-        pythonEnv = self.packages.${system}.default.dependencyEnv;
+        pythonEnv = self.packages.${system}.abiopetaja-tests.dependencyEnv;
       in
       {
         settings = {
--- flake/packages.nix
+++ flake/packages.nix
@@ -7,6 +7,12 @@
         python = pkgs.python312;
       };
 
+      abiopetaja-tests = pkgs.poetry2nix.mkPoetryApplication {
+        projectDir = ../.;
+        python = pkgs.python312;
+        groups = ["tests"];
+      };
+
       default = self.packages.${system}.abiopetaja;
     };
   };
