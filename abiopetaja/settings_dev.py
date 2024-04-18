5e4875c2deb7ae13b8420451e7465fcc2bad4b4d
Gregor Grigorjan <gregor@grigorjan.net>
2024-04-18 14:22:23 +0300

    tweak(settings): `dev`: allow all hosts

--- abiopetaja/settings_dev.py
+++ abiopetaja/settings_dev.py
@@ -5,7 +5,7 @@ SECRET_KEY = "django-insecure-4k9w_ulf47yv^#^d(one)im0x7)sc7k9d_rcm3o+b00r1ri*fx
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "192.168.0.15", "192.168.0.10"]
+ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     "default": {
