4e98fe70a58ed48e3a360bbe92a301334b6992c0
Juss Põldsam <jupold@ttu.ee>
2024-04-19 22:30:54 +0300

    feat(tests): add create and update tests for template `title` field

--- app/tests.py
+++ app/tests.py
@@ -89,6 +89,38 @@ def test_created_template_has_input_name(client: Client):
 
 
 @pytest.mark.django_db
+def test_created_template_has_input_fields(client: Client):
+    template_title = "My title"
+    user = create_user(client)
+    client.force_login(user)
+
+    client.post(reverse("app:template-create"), {"name": "My template", "title": template_title})
+
+    template: Template = Template.objects.get(author=user, name="My template")
+    assert template.title == template_title
+
+
+@pytest.mark.django_db
+def test_updated_template_has_updated_fields(client: Client):
+    template_title = "My title"
+    updated_title = "Updated title"
+    user = create_user(client)
+    client.force_login(user)
+
+    client.post(reverse("app:template-create"), {"name": "My template", "title": template_title})
+
+    template: Template = Template.objects.get(author=user, name="My template")
+    assert template.title == template_title
+
+    client.post(
+        reverse("app:template-update", kwargs={"pk": template.pk}), {"title": updated_title}
+    )
+
+    template: Template = Template.objects.get(author=user, name="My template")
+    assert template.title == updated_title
+
+
+@pytest.mark.django_db
 def test_user_can_get_created_template(client: Client):
     user = create_user(client)
     client.force_login(user)
