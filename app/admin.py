8b07686418b14d79f9098c6b21d92e15703c3424
Gregor Grigorjan <gregor@grigorjan.net>
2024-05-09 20:21:03 +0300

    fix(admin): register `UserFeedback`

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 
-from app.models import Template, TemplateProblem, Test
+from app.models import Template, TemplateProblem, Test, UserFeedback
 
-admin.site.register(Template)
-admin.site.register(TemplateProblem)
-admin.site.register(Test)
+
+class EntityAdmin(admin.ModelAdmin):
+    readonly_fields = ("created_at", "updated_at")
+
+
+def register_entity(entity):
+    admin.site.register(entity, EntityAdmin)
+
+
+register_entity(Template)
+register_entity(TemplateProblem)
+register_entity(Test)
+register_entity(UserFeedback)
