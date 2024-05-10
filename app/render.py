134021b29db0f95ec5f03603a0047a05d0d66cde
JussPoldsam <47038005+JussPoldsam@users.noreply.github.com>
2024-05-10 13:10:58 +0300

    feat(render): add problem numbers (#27)
    * feat(render): add index numbers to test version rendering
    
    * feat(render): add index numbers to answer key rendering
    
    * tweak(render): code cleanup

--- app/render.py
+++ app/render.py
@@ -1,11 +1,32 @@
+import string
+
 from django.utils.translation import gettext_lazy as _
-from django.utils.translation import pgettext
 
 from app.models import (
     Problem,
     Test,
     TestVersion,
 )
+
+
+def _char_from_int(index: int) -> str:
+    """
+    Convert number to corresponding letter.
+
+    Exmaple:
+    0 -> a
+    1 -> b
+    ...
+    """
+    return string.ascii_lowercase[index]
+
+
+def _get_problems_by_kind(problems: list[Problem]) -> dict[int, list[Problem]]:
+    problem_kind_set = {problem.kind for problem in problems}
+    return {
+        problem_kind: [problem for problem in problems if problem.kind == problem_kind]
+        for problem_kind in problem_kind_set
+    }
 
 
 def _make_document(source: str) -> str:
@@ -26,34 +47,33 @@ def _make_document(source: str) -> str:
 
 
 def _render_problems(problems: list[Problem]) -> str:
-    problem_kind_set = {problem.kind for problem in problems}
-    problems_by_kind = {
-        problem_kind: [problem for problem in problems if problem.kind == problem_kind]
-        for problem_kind in problem_kind_set
-    }
+    problems_by_kind = _get_problems_by_kind(problems)
+    problem_kind_list = list(problems_by_kind.keys())
 
     return f"""
-    {"\n".join(_render_problem_kind(problems_by_kind[kind]) for kind in problems_by_kind)}
+    {"\n".join(_render_problem_kind(problems_by_kind[problem_kind_list[i]], i)
+        for i in range(len(problem_kind_list)))}
     """
 
 
-def _render_problem_kind(problems: list[Problem]) -> str:
+def _render_problem_kind(problems: list[Problem], subproblem_index: int) -> str:
     assert len(set(problem.kind for problem in problems)) == 1
     problem_text = problems[0].problem_text
 
     return f"""
     \\noindent
-    {problem_text}\\newline
-    {"\\newline".join(f"${problem.definition}$" for problem in problems)}
+    {subproblem_index + 1}) {problem_text}\\newline \\indent
+    {"\\newline \\indent".join(f" {_char_from_int(i)}) ${problems[i].definition}$"
+        for i in range(len(problems)))}
     """
 
 
-def _render_header(title: str, version: int | None) -> str:
+def _render_header(title: str, subtitle: str) -> str:
     return f"""
     \\begin{{center}}
     {"" if title == "" else f"{{\\Large \\textbf{{{title}}}}}"}
 
-    {"" if version is None else f"{_("Version")} {version}"}
+    {subtitle}
     \\end{{center}}
     """
 
@@ -64,7 +84,7 @@ def render_test_version(version: TestVersion) -> str:
 
     latex = _make_document(
         f"""
-        {_render_header(test.title, version.version_number)}
+        {_render_header(test.title, _("Version %(version)s") % {"version": version.version_number})}
         {_render_problems(problems)}
         """
     )
@@ -72,22 +92,34 @@ def render_test_version(version: TestVersion) -> str:
     return latex
 
 
-def _render_answer(problem: Problem) -> str:
-    return f"\n\\textbf{{{pgettext("for a math problem", "Solution")}}}: ${problem.solution}$\n"
+def _render_problem_kind_answer(problems: list[Problem], subproblem_index: int) -> str:
+    assert len(set(problem.kind for problem in problems)) == 1
+    problem_text = problems[0].problem_text
+
+    return f"""
+    \\noindent
+    {subproblem_index + 1}) {problem_text}\\newline \\indent
+    {"\\newline \\indent".join(f" {_char_from_int(i)}) ${problems[i].solution}$"
+        for i in range(len(problems)))}
+    """
 
 
 def _render_test_version_answers(version: TestVersion) -> str:
-    version_label = _("Version") + f" {version.version_number}"
+    version_label = _("Version %(version)s") % {"version": version.version_number}
+    problems_by_kind = _get_problems_by_kind(list(version.problem_set.all()))
+    problem_kind_list = list(problems_by_kind.keys())
+
     return f"""
     \\subsection*{{{version_label}}}
-    {"\\newline".join(_render_answer(problem) for problem in version.problem_set.all())}
+    {"\n".join(_render_problem_kind_answer(problems_by_kind[problem_kind_list[i]], i)
+        for i in range(len(problem_kind_list)))}
     """
 
 
 def render_answer_key(test: Test) -> str:
     latex = _make_document(
         f"""
-        {_render_header(test.title, version=None)}
+        {_render_header(test.title, _("Answer Key"))}
         {"\n".join(_render_test_version_answers(version) for version in test.versions)}
         """
     )
