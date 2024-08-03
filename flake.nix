7a4cc20978315089ad5d87fc59681f71065eefd2
Gregor Grigorjan <gregor@grigorjan.net>
2024-08-03 14:02:22 +0300

    refactor(flake): inputs: remove follows

--- flake.nix
+++ flake.nix
@@ -5,13 +5,10 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
 
     poetry2nix.url = "github:nix-community/poetry2nix";
-    poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
 
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
-    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
 
     deploy-rs.url = "github:serokell/deploy-rs";
-    deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs: import ./flake inputs;
